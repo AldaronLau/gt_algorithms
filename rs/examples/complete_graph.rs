60743458ae9c650c09f885e049fa054bee28b19c
Jeron Aldaron Lau <jeron.lau@plopgrizzly.tech>
2017-04-12 22:18:18 -0500

    Implemented generating complete graphs represented by vertices.

--- rs/examples/complete_graph.rs
+++ rs/examples/complete_graph.rs
@@ -1,12 +1,12 @@
 extern crate gt_algorithms;
 extern crate rayon;
 
-use gt_algorithms::{ Graph, Edge };
+use gt_algorithms::{ VertexGraph, EdgeGraph, Edge };
 use rayon::iter::IntoParallelRefIterator;
 use rayon::prelude::*;
 
 pub fn main() {
-	let cg = Graph::complete(3).edges;
+	let cg = EdgeGraph::complete(3).edges;
 	let a = cg.par_iter().map(|a| {
 		let string : String = format!("{:?}", a.adjacents);
 		string
@@ -18,6 +18,12 @@ pub fn main() {
 	for i in 0..10 {
 		let j = i + 1;
 		println!("Edges For {} Vertices: {}", j, Edge::count_for_k(j));
+	}
+
+	let size = 8;
+	let vertexgraph = VertexGraph::complete(size).vertices;
+	for i in 0..size {
+		println!("Vertices: {:?}", vertexgraph[i].adjacents );
 	}
 //		.reduce(
 //			|a| { println!("aa {:?}", a.adjacents); (0, 0) },
--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,13 +1,22 @@
 extern crate rayon;
 
-// use rayon::prelude::*;
+use rayon::prelude::*;
+// use rayon::iter::IntoParallelRefIterator;
+
+pub struct Vertex {
+	pub adjacents: Vec<usize>,
+}
 
 pub struct Edge {
 	pub adjacents: Vec<usize>,
 }
 
-pub struct Graph {
-	pub edges: Vec<Edge>
+pub struct VertexGraph {
+	pub vertices: Vec<Vertex>,
+}
+
+pub struct EdgeGraph {
+	pub edges: Vec<Edge>,
 }
 
 impl Edge {
@@ -17,15 +26,45 @@ impl Edge {
 	}
 }
 
-impl Graph {
-	pub fn complete(size: usize) -> Graph {
+impl VertexGraph {
+	pub fn complete(size: usize) -> VertexGraph {
+//		let s = Edge::count_for_k(size);
+//		let v = ;
+		let vertices = (0..size).into_par_iter().map(|a| {
+			let mut adjacents = Vec::new();
+			// TODO: Figure out which algorithm is faster.
+
+			// Algorithm A
+//			for i in 0..size {
+//				if i != a {
+//					adjacents.push(i);
+//				}
+//			}
+
+			// Algorithm B
+			for i in 0..a {
+				adjacents.push(i);
+			}
+			for i in (a+1)..size {
+				adjacents.push(i);
+			}
+
+			Vertex { adjacents: adjacents }
+		}).collect();
+
+		VertexGraph { vertices: vertices }
+	}
+}
+
+impl EdgeGraph {
+	pub fn complete(size: usize) -> EdgeGraph {
 		let mut edges = Vec::new();
 		let mut adjacents = Vec::new();
 	
 		adjacents.push(1);
 		adjacents.push(2);
 		edges.push(Edge { adjacents: adjacents });
-		Graph { edges: edges }
+		EdgeGraph { edges: edges }
 	}
 }
 
